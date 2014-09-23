852adbc9d34d7fdabb34a79407d9f12ba20abd80
Ryan Eagan <eaganry@gmail.com>
2014-09-22 22:15:03 -0400

    Charts for stories using chart.js

--- story/urls.py
+++ story/urls.py
@@ -6,4 +6,5 @@ from story.models import Story
 urlpatterns = patterns('',
     url(r'^(?P<slug>\w+)/$', views.story, name='story'),
     url(r'^(?P<slug>\w+)/(?P<headline>\d+)/$', views.headline, name='headline'),
+    url(r'^line_chart/json/$', views.line_chart_json, name='line_chart_json'),
 )
--- story/views.py
+++ story/views.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.shortcuts import render
-from story.models import Story, Keyword, Headline, HeadlineLink
+from story.models import Story, Keyword, Headline, HeadlineLink, HourCount
 
 # Create your views here.
 
@@ -24,3 +26,35 @@ def headline(request, slug, headline):
 		context["error"] = "Not Found"
 
 	return render(request, 'story/headline.html', context)
+
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
+import pdb;
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+    	current_hour = datetime.datetime.now().hour
+    	labels = []
+    	for hour in range(0,24):
+    		if (current_hour+hour)%24 <= 12:
+    			labels.append(str((current_hour+hour)%24) + " am")
+    		else:
+    			labels.append(str((current_hour+hour)%24-12) + " pm")
+    	print labels
+        return labels
+
+    def get_data(self):
+    	data = []
+    	for story in Story.objects.all():
+    		sub_data = [0]*24
+    		start_time = datetime.datetime.now() + datetime.timedelta(hours=-24)
+    		start_time = start_time.replace(minute=0, second=0)
+    		hours = HourCount.objects.filter(story=story, date__gte=start_time)
+    		for hour in hours:
+    			sub_data[(hour.date.hour-start_time.hour)%24] = hour.count()
+    		data.append(sub_data)
+		return data
+
+
+line_chart = TemplateView.as_view()
+line_chart_json = LineChartJSONView.as_view()
